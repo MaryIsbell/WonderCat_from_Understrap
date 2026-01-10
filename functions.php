7272751c32600413cdc1ae8a1a2b5bcf34695771
Mary Isbell <mary.isbell@gmail.com>
2026-01-10 14:16:44 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -86,3 +86,6 @@ function understrap_child_customize_controls_js() {
 	);
 }
 add_action( 'customize_controls_enqueue_scripts', 'understrap_child_customize_controls_js' );
+
+//loads inc contents
+require_once get_theme_file_path( 'inc/acf.php' );
