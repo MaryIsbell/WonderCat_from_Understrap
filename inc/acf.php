51d07c9963a9fca4da5aee008a6a9c1162c9c8db
Mary Isbell <mary.isbell@gmail.com>
2026-01-13 14:47:58 -0500

    removing earlier version
    I had commented it out, but now deleting

--- inc/acf.php
+++ inc/acf.php
@@ -186,67 +186,6 @@ function cat_tech_update_populate_type( $form ) {
 
 
 
-
-
-
-
-
-
-
-
-
-// NOTE: update the '1' to the ID of your form
-/* add_filter( 'gform_pre_render_1', 'cat_tech_update_populate_type_experience' );
-add_filter( 'gform_pre_validation_1', 'cat_tech_update_populate_type_experience' );
-add_filter( 'gform_pre_submission_filter_1', 'cat_tech_update_populate_type_experience' );
-add_filter( 'gform_admin_pre_render_1', 'cat_tech_update_populate_type_experience' );
-function cat_tech_update_populate_type_experience( $form ) {
-  
-    foreach( $form['fields'] as &$field )  {
-  
-        //NOTE: replace 5 with your checkbox field id
-        $field_id = 4;
-        //NOTE: replace 'technology' with your custom taxonomy name
-        $custom_taxonomy = 'experience';
-        if ( $field->id != $field_id ) {
-            continue;
-        }
-  
-        $terms = get_terms( array(
-                'taxonomy' =>  $custom_taxonomy,
-                'hide_empty' => false,
-                'orderby'   =>'title',
-                'order'   =>'ASC',
-            ) ); 
-        $input_id = 1;
-        foreach( $terms as $term ) {
-  
-            // Check if 'in_glossary' meta value is 'yes'
-            $in_glossary = get_term_meta( $term->term_id, 'in_glossary', true );
-            if ( strtolower($in_glossary) !== 'yes' ) {
-                continue;
-            }
-
-            //skipping index that are multiples of 10 (multiples of 10 create problems as the input IDs)
-            if ( $input_id % 10 == 0 ) {
-                $input_id++;
-            }
-  
-            $choices[] = array( 'text' => $term->name, 'value' => $term->name);
-            $inputs[] = array( 'label' => $term->name, 'id' => "{$field_id}.{$input_id}");
-  
-            $input_id++;
-        }
-//   array_push($field->choices,"i-want-to-propose-a-new-term","i-need-help-deciding");
-//         $field->choices = $choices;
-//         $field->inputs = $inputs;
-  
-    }
-  
-    return $form;
-} */
-
-
 //save acf json
 add_filter('acf/settings/save_json', 'wondercat_json_save_point');
  
