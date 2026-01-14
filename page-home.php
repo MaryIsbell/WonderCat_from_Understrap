38e4e2c14401e26b342594fe51a5efbaa228b25d
Mary Isbell <mary.isbell@gmail.com>
2026-01-14 13:55:50 -0500

    build home page template

---
+++ page-home.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * A custom template to associate with a static home page for the WonderCat site that draws in featured user-experience posts into a carousel
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+?>
+
+<main id="primary" class="site-main">
+
+    <section class="home-hero">
+        <h1>Homepage Template Loaded</h1>
+        <p>If you see this, your template is working.</p>
+    </section>
+
+</main>
+
+<?php
+get_footer();
+
