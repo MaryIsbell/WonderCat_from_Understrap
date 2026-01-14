c3b63c63577cb824d9a48249005091010b6bf273
Mary Isbell <mary.isbell@gmail.com>
2026-01-14 09:19:53 -0500

    footer tweaks
    trying to see the content in the viewport

--- footer.php
+++ footer.php
@@ -2,46 +2,34 @@
 /**
  * The template for displaying the footer
  *
- * Contains the closing of the #content div and all content after
- *
  * @package Understrap
  */
 
-// Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-
-$container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <div class="wrapper" id="wrapper-footer">
 
-    <div class="<?php echo esc_attr( $container ); ?>">
+    <footer class="site-footer wc-footer-bar">
 
-        <div class="row">
+        <div class="wc-footer-inner">
 
-            <div class="col-md-12">
+            <div class="wc-footer-left">
+                <a href="<?php echo esc_url( home_url('/about') ); ?>">About</a>
+            </div>
 
-                <footer class="site-footer wc-footer-bar">
-
-                    <div class="wc-footer-inner">
-
-                        <div class="wc-footer-left">
-                            <a href="<?php echo esc_url( home_url('/about') ); ?>">About</a>
-                        </div>
-
-                        <div class="wc-footer-right">
-                            Powered by WordPress and WikiData | WonderCat Theme
-                        </div>
-
-                    </div>
-
-                </footer>
-
+            <div class="wc-footer-right">
+                Powered by WordPress and WikiData | WonderCat Theme
             </div>
 
         </div>
 
-    </div>
+    </footer>
 
 </div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
+
 
