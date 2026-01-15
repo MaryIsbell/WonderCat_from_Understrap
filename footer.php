d47bf35ef3edfdee50f4b2838b0ee073cd9e2c48
Mary Isbell <mary.isbell@gmail.com>
2026-01-15 14:35:21 -0500

    footer adjustments

--- footer.php
+++ footer.php
@@ -8,24 +8,16 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<div class="wrapper" id="wrapper-footer">
-
-    <footer class="site-footer wc-footer-bar">
-
-        <div class="wc-footer-inner">
-
-            <div class="wc-footer-left">
-                <a href="<?php echo esc_url( home_url('/about') ); ?>">About</a>
-            </div>
-
-            <div class="wc-footer-right">
-                Powered by WordPress and WikiData | WonderCat Theme
-            </div>
-
-        </div>
-
-    </footer>
-
+<div class="wrapper" id="wrapper-footer"> 
+    <footer class="site-footer wc-footer-bar"> 
+        <div class="container-fluid"> 
+            <div class="row justify-content-between align-items-center"> 
+                <div class="col-auto wc-footer-left"> 
+                    <a href="<?php echo esc_url( home_url('/about') ); ?>">About</a> 
+                </div> <div class="col-auto wc-footer-right text-end"> Powered by WordPress and WikiData | WonderCat Theme </div> 
+            </div> 
+        </div> 
+    </footer> 
 </div>
 
 <?php wp_footer(); ?>
