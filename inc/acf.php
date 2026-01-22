b7f9874df0357dcaf0c56bbe7642a8b95e9ee1c1
Mary Isbell <mary.isbell@gmail.com>
2026-01-22 08:57:28 -0500

    removing early iteration of get_terms
    that included array_push that might be an issue

--- inc/acf.php
+++ inc/acf.php
@@ -121,67 +121,6 @@ function cat_exp_populate_dropdown( $form ) {
 
 
 
-
-/**
- * ORIGINAL CODE FROM TOM, COMMENTED OUT TO TEST NEW CODE
- * tie taxonomy to form field
- * 
- **/
-/* // NOTE: update the '1' to the ID of your form
-add_filter( 'gform_pre_render_1', 'cat_tech_update_populate_type' );
-add_filter( 'gform_pre_validation_1', 'cat_tech_update_populate_type' );
-add_filter( 'gform_pre_submission_filter_1', 'cat_tech_update_populate_type' );
-add_filter( 'gform_admin_pre_render_1', 'cat_tech_update_populate_type' );
-function cat_tech_update_populate_type( $form ) {
-  
-    foreach( $form['fields'] as &$field )  {
-  
-        //NOTE: replace 5 with your checkbox field id
-        $field_id = 5;
-        //NOTE: replace 'technology' with your custom taxonomy name
-        $custom_taxonomy = 'technology';
-        if ( $field->id != $field_id ) {
-            continue;
-        }
-  
-        $terms = get_terms( array(
-                'taxonomy' =>  $custom_taxonomy,
-                'hide_empty' => false,
-                'orderby'   =>'title',
-                'order'   =>'ASC',
-            ) ); 
-        $input_id = 1;
-        foreach( $terms as $term ) {
-            
-            // Check if 'in_glossary' meta value is 'yes'
-            $in_glossary = get_term_meta( $term->term_id, 'in_glossary', true );
-            if ( strtolower($in_glossary) !== 'yes' ) {
-                continue;
-            }
-  
-            //skipping index that are multiples of 10 (multiples of 10 create problems as the input IDs)
-            if ( $input_id % 10 == 0 ) {
-                $input_id++;
-            }
-  
-            $choices[] = array( 'text' => $term->name, 'value' => $term->name);
-            $inputs[] = array( 'label' => $term->name, 'id' => "{$field_id}.{$input_id}");
-  
-            $input_id++;
-        }
-        array_push($field->choices,"i-want-to-propose-a-new-term","i-need-help-deciding");
-        
-  
-        $field->choices = $choices;
-        $field->inputs = $inputs;
-  
-    }
-  
-    return $form;
-} */
-
-
-
 //save acf json
 add_filter('acf/settings/save_json', 'wondercat_json_save_point');
  
